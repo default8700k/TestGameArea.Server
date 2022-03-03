54d9afb276cce10f0148a757eece1930d1989c74
Victor Maltsev <default8700k@gmail.com>
2022-03-03 19:26:53 +0300

    Add project files.

---
+++ WebApplication/RequestHandler.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication
+{
+    public class RequestHandler : Hub
+    {
+
+    }
+}
---
+++ WebApplication/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<RequestHandler>("/request");
+            });
+        }
+    }
+}
